d3bd00915d15922de27ddadf2e180f0be105a6cd
Mandeep Sidhu <mandeep.sidhu2@gmail.com>
2020-06-14 23:47:10 +0530

    script changes

--- index.js
+++ index.js
@@ -1,15 +1,10 @@
 const cheerio = require('cheerio');
 const request = require('request');
 const express = require('express');
-const exphbs = require('express-handlebars');
-const bodyParser = require('body-parser');
+
+
 const app = express();
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended : false}));
-
-app.engine('handlebars', exphbs({ defaultLayout : 'main'}));
-app.set('view engine','handlebars');
 
 app.listen(process.env.PORT || 8080);
 
